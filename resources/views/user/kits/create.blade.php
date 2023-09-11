16911af97edc725453187c05e0b15775989e3252
khaironbiz <khaironbiz@gmail.com>
2023-09-11 14:41:49 +0700

    kits

--- resources/views/user/kits/create.blade.php
+++ resources/views/user/kits/create.blade.php
@@ -4,9 +4,22 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
+                    @if(session('danger'))
+                        <div class="alert alert-danger" role="alert">
+                            {{ session('danger') }}
+                        </div>
+                    @endif
+                    @if(session('success'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('success') }}
+                        </div>
+                    @endif
                     <div class="card">
                         <div class="card-header bg-info">
-                            @include('layout.menu.admin.submenu.master')
+                            @if(\Illuminate\Support\Facades\Auth::user()->is_admin == true)
+                                @include('layout.menu.admin.submenu.master')
+                            @endif
+
                         </div>
                         <div class="row mt-2 justify-content-center">
                             <div class="col-lg-8">
@@ -16,7 +29,11 @@
                                         @csrf
                                         @include('user.kits._form')
                                         <diV class="card-footer">
-                                            <a href="{{ route('kits.index') }}" class="btn btn-warning">Back</a>
+                                            @if(\Illuminate\Support\Facades\Auth::user()->is_admin == true)
+                                                <a href="{{ route('kits.index') }}" class="btn btn-warning">Back</a>
+                                            @else
+                                                <a href="{{ route('customers.show',['id'=>\Illuminate\Support\Facades\Auth::user()->organisasi['id']]) }}" class="btn btn-warning">Back</a>
+                                            @endif
                                             <button type="submit" class="btn btn-primary">Save</button>
                                         </diV>
                                     </form>
